3c4bb676970d8eea85b8255df51cbbe8364ccfe4
shivarajkumar <shivaraj1654@gmail.com>
2023-01-31 14:40:28 +0530

    added key value to thhe items

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -17,11 +17,9 @@ const ExpenseItem = (props) => {
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>${amount}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change title</button>
-      <button onClick={clickAmount}>Change Amount</button>
     </Card>
   );
 }
